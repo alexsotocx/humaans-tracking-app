fe59b6dc59852819ac3f6bd8c80d3e43d81d8d75
Alexander Soto <alexsotocx@gmail.com>
2023-01-31 21:43:33 +0100

    Add husky and lint

---
+++ src/repositories/humaans.ts
@@ -0,0 +1,13 @@
+export type HumaansTimeTrackingEntry = {
+  id: "0aPyFLntRCllm0EJ6KL8Okpv";
+  personId: "ZS9fF49qALjgHhoth55B0r35";
+  date: "2023-01-13";
+  startTime: "13:33:00";
+  endTime: "16:30:00";
+  duration: {
+    hours: 2;
+    minutes: 57;
+  };
+  createdAt: "2023-01-13T13:41:06.547Z";
+  updatedAt: "2023-01-13T15:30:17.183Z";
+};
